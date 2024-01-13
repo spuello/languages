0a990d36ccca54be51acc11704ddd358176bfb3d
Samuel Puello <spuellocoronado@gmail.com>
2024-01-13 12:51:33 -0400

    refer stack and pointers

---
+++ go/030_pointers/code.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// The type *T is a pointer to a T value. Its zero value is nil.
+// The & operator generates a pointer to its operand
+// The * operator denotes the pointer's underlying value.
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(*p)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
+}
