4ebfcc7fbfa1819597e03c2e11a93794df491bf2
Samuel Puello <spuellocoronado@gmail.com>
2024-02-10 14:12:34 -0400

    Swift: Strings and Integers basic types

---
+++ swift/strings_integers.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+//Swift is a type-safe language, requiring variables to be of a specific type.
+
+var greeting = "Hello, playground"
+var age = 38
+//Large number with underscore as thousand seperator
+var population = 8_000_000
+
