64d61dbe0f43b53d72a0788f689b59282674b93e
Samuel Puello <spuellocoronado@gmail.com>
2024-02-10 12:12:12 -0400

    kotlin: String templates

---
+++ kotlin/string_templates/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val customers = 10
+    println("There are $customers customers in the database")
+    println("Tomorrow, there will be ${customers+1} customers in the database")
+}
