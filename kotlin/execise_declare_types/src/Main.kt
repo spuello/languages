0f84eb72a7e28378feb97678c1b7f2291a412db9
Samuel Puello <spuellocoronado@gmail.com>
2024-02-10 13:12:07 -0400

    kotlin: exercise declaring values with basice types

---
+++ kotlin/execise_declare_types/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+    println("The types of these values are: Int, String, Double, Long, Boolean, Char")
+}
