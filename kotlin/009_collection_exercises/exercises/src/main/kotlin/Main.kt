752c5dc86cc09b25d75c8d8887e0ebe6659db5f9
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 11:24:25 -0400

    WIP

--- kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
+++ kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
@@ -3,10 +3,6 @@ package org.example
 //TODO #1: You have a list of “green” numbers and a list of “red” numbers.
 // Complete the code to print how many numbers there are in total.
 
-fun main() {
-    println("Hello World!")
-}
-
 class Sample() {
     fun sum(a: Int, b: Int): Int {
         return a + b
