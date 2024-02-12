c688aae5d698f34fd821689d811c5d03f862d7ad
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 11:38:46 -0400

    WIP

--- kotlin/009_collection_exercises/exercises/src/test/kotlin/CollectionExercisesTest.kt
+++ kotlin/009_collection_exercises/exercises/src/test/kotlin/CollectionExercisesTest.kt
@@ -4,21 +4,12 @@ import org.junit.jupiter.api.Test
 
 class CollectionExercisesTest {
     private val sut: CollectionExercises = CollectionExercises()
-
-  
-
-
+    
     @Test
     fun testListsAmountOfNumbers() {
         val expected = 5
         val result = sut.amountOfNumbers()
-
-        kotlin.test.assertEquals(
-            expected, result
-        )
-
+        assertEquals(expected, result)
     }
-
-
 }
 
