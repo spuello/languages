80e0df38b045259fcad240e1ef2ce1292e3dfe47
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 07:14:40 -0400

    Kotlin: collection list

--- kotlin/006_collection_list/collection_list/src/Main.kt
+++ kotlin/006_collection_list/collection_list/src/Main.kt
@@ -3,5 +3,12 @@ val shapes: MutableList<String> = mutableListOf("triangle")
 fun main() {
     shapes.add("square")
     println(readOnlyShapes)
-    println("Mutable list of shapes: $shapes" )
+    println("Mutable list of shapes: $shapes")
+    println("The first item in the read-only list is ${readOnlyShapes[0]}")
+    println("This is the count of the read-only list: ${readOnlyShapes.count()}")
+    print("The circle in the list:  ${"circle" in readOnlyShapes}")
+    print("Removing one item from the list")
+    shapes.remove("square")
+    println()
+
 }
