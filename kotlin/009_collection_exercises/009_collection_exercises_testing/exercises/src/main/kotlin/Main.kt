a2d1e468098f6470fc4b6534f6062d8d80f9dadd
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 08:12:15 -0400

    WIP

--- kotlin/009_collection_exercises/009_collection_exercises_testing/exercises/src/main/kotlin/Main.kt
+++ kotlin/009_collection_exercises/009_collection_exercises_testing/exercises/src/main/kotlin/Main.kt
@@ -2,8 +2,29 @@ package org.example
 
 
 class CollectionExercises() {
+    //TODO #1: You have a list of “green” numbers and a list of “red” numbers.
+    // Complete the code to print how many numbers there are in total.
     fun sum(a: Int, b: Int): Int {
         return a + b
     }
 
 }
+
+//fun main() {
+//    val greenNumbers = listOf(1, 4, 23)
+//    val redNumbers = listOf(17, 2)
+//    println("Total: ${greenNumbers.count() + redNumbers.count()}")
+//}
+//
+//TODO #1: You have a list of “green” numbers and a list of “red” numbers.
+// Complete the code to print how many numbers there are in total.
+//fun one() {
+//
+//}
+//
+////TODO #2: You have a set of protocols supported by your server. A user requests to use a particular protocol.
+//// Complete the program to check whether the requested protocol
+//// is supported or not (isSupported must be a Boolean value).
+//fun tow() {
+//
+//}
