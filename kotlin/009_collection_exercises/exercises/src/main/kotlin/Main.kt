813d18f2e666967cb833d8aa3af9082c0db613dc
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 11:37:35 -0400

    WIP

--- kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
+++ kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
@@ -9,6 +9,8 @@ class CollectionExercises() {
     }
 
     fun amountOfNumbers(): Int {
-        return 5
+        val greenNumbers = listOf(1, 4, 23)
+        val redNumbers = listOf(17, 2)
+        return greenNumbers.count() + redNumbers.count()
     }
 }
