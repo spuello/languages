82fb7b18d95335fbbe52d43d1f05e3bac2eb2a95
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 08:15:52 -0400

    WIP

--- kotlin/009_collection_exercises/009_collection_exercises_testing/exercises/src/test/kotlin/CollectionExercisesTest.kt
+++ kotlin/009_collection_exercises/009_collection_exercises_testing/exercises/src/test/kotlin/CollectionExercisesTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 internal class CollectionExercisesTest {
     private val sut: CollectionExercises = CollectionExercises()
     @Test
-    fun testSum() {
+    fun testQuantityOfNumbers() {
         val expected = 5
         kotlin.test.assertEquals(expected, sut.quantityOfNumbers())
     }
