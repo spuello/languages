a914fc787f7ef0c8d98739067389c9b2cf14dcbf
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 10:57:08 -0400

    WIP

---
+++ kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//TODO #1: You have a list of “green” numbers and a list of “red” numbers.
+// Complete the code to print how many numbers there are in total.
+
+fun main() {
+    println("Hello World!")
+}
+
+class Sample (){}
---
+++ kotlin/009_collection_exercises/exercises/src/test/kotlin/SampleTest.kt
@@ -0,0 +1,14 @@
+import org.example.Sample
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+    private val sut: Sample = Sample()
+
+    @Test
+    fun testSum(){
+
+    }
+
+}
+
