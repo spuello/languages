751b49f21321847939cf2c0f4b0a501071a0b26b
Samuel Puello <spuellocoronado@gmail.com>
2024-02-12 11:39:16 -0400

    WIP

--- kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
+++ kotlin/009_collection_exercises/exercises/src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
 package org.example
 
-//TODO #1: You have a list of “green” numbers and a list of “red” numbers.
-// Complete the code to print how many numbers there are in total.
-
 class CollectionExercises() {
     fun amountOfNumbers(): Int {
         val greenNumbers = listOf(1, 4, 23)
