94510e901181acfd7d6b8a2d2ed3db9320f05797
Samuel Puello <spuellocoronado@gmail.com>
2024-02-20 06:36:38 -0400

    Initial swift console application

--- swift/008_commandline_app/Package.swift
+++ swift/008_commandline_app/Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "008_commandline_app",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+    targets: [        // Targets are the basic building blocks of a package, defining a module or a test suite.
         .executableTarget(
             name: "008_commandline_app"),
     ]
