85d95cfc289e5503aeb4c1cf6fba734de5e24069
Samuel Puello <spuellocoronado@gmail.com>
2024-06-30 18:20:36 -0400

    Kotlin: Comments

---
+++ kotlin/C004Comments/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!")
+
+    // This is an end-of-line comment
+
+    /* This is block
+     on multiple lines*/
+
+    /* The comment starts here
+    /*contains a nested comment*/
+
+     */
+}
